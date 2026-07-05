69219fb62437bf56b45d1fb269fa201eb41c8858
DrStrange456 <75033117+DrStrange456@users.noreply.github.com>
2026-07-05 18:50:29 -0400

    jkljkl

--- GameFiles/Test/wave_manager.gd
+++ GameFiles/Test/wave_manager.gd
@@ -30,32 +30,31 @@ var wave_active := false
 
 
 const WAVES = [
+	#{
+		#"time_limit":30.0,
+		#"delay": 3.0,
+		#"enemies": [
+			#{"scene": preload("res://characters/mud_man/mud_man.tscn"), 
+			#"count": 5},
+		#]
+	#},
 	{
-		"time_limit":30.0,
-		"delay": 3.0,
-		"enemies": [
-			#{"scene": preload("res://characters/bat/bat.tscn"), "count": 5},
-			{"scene": preload("res://characters/mud_man/mud_man.tscn"), 
-			"count": 5},
-		]
-	},
-	{
-		"time_limit": 40.0,
+		"time_limit": 60.0,
 		"delay": 3.0,
 		"enemies": [
 			{"scene": preload("res://characters/bat/bat.tscn"), "count": 8},
 			{"scene": preload("res://characters/bat/bat.tscn"), "count": 2},
 		]
 	},
-	{
-		"time_limit": 50.0,
-		"delay": 3.0,
-		"enemies": [
-			{"scene": preload("res://characters/bat/bat.tscn"), "count": 10},
-			{"scene": preload("res://characters/bat/bat.tscn"), "count": 8},
-			{"scene": preload("res://characters/bat/bat.tscn"), "count": 2},
-		]
-	},
+	#{
+		#"time_limit": 50.0,
+		#"delay": 3.0,
+		#"enemies": [
+			#{"scene": preload("res://characters/bat/bat.tscn"), "count": 10},
+			#{"scene": preload("res://characters/bat/bat.tscn"), "count": 8},
+			#{"scene": preload("res://characters/bat/bat.tscn"), "count": 2},
+		#]
+	#},
 ]
 
 
