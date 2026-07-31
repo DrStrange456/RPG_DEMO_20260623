9d3b811d8df0c95198beda39b2b68b603cfd3ece
DrStrange456 <75033117+DrStrange456@users.noreply.github.com>
2026-07-30 21:06:24 -0400

    huinmnnk

--- GameFiles/scenes/machines/seed_maker.gd
+++ GameFiles/scenes/machines/seed_maker.gd
@@ -1,53 +1,135 @@
 extends StaticBody2D
 
+signal processing_started
+signal seed_produced
+signal processing_finished
+signal seeds_collected
+signal state_changed
+
 @export var id: int
+@export var process_time := 10.0
+
 @onready var interact_icon: Sprite2D = $imgIcon
-var player_within_range: bool = false
+@onready var timer: Timer = $Timer
+
+var player_within_range := false
 var plyr
 
+# TODO: Replace with your recipe lookup.
 var output_object = preload("res://resources/seeds_carrot.tres")
-
 
 enum State {
 	IDLE,
-	PROCESSING,
-	FINISHED
+	PROCESSING
 }
 
-var state = State.IDLE
+var state: State = State.IDLE
 
-var input_crop
-var output_seed
-var output_amount
+var input_crop = null
+var output_seed = null
 
-@onready var timer = $Timer
+# Number of crops still waiting to process.
+var crops_remaining := 0
+
+# Number of finished seed packs waiting to be collected.
+var seeds_ready := 0
 
 
-func start_processing(crop):
+func start_processing(crop, amount: int) -> void:
 
-	if state != State.IDLE:
+	if amount <= 0:
 		return
-	
+
+	# Already processing
+	if state == State.PROCESSING:
+
+		# Allow adding more of the same crop.
+		if crop == input_crop:
+			crops_remaining += amount
+			print("Added ", amount, " more crops.")
+			emit_signal("state_changed")
+			return
+
+		print("Machine is already processing another crop.")
+		return
+
 	input_crop = crop
-	state = State.PROCESSING
-	
-	timer.start(2)
-
-func _on_timer_timeout():
-	#output_seed = recipes[input_crop.id].seed
 	output_seed = output_object
-	output_amount = randi_range(1,3)
-	print("outputting seed pack: ", output_amount)
-	
-	state = State.FINISHED
 
-func collect():
-	print("collecting seed:")
-	state = State.IDLE
+	crops_remaining = amount
+	seeds_ready = 0
 
-func add_seeds():
-	# Try to add seeds to inventory
-	pass
+	state = State.PROCESSING
+
+	timer.start(process_time)
+
+	emit_signal("processing_started")
+	emit_signal("state_changed")
+
+
+func _on_timer_timeout() -> void:
+
+	if crops_remaining <= 0:
+		return
+
+	# Consume one crop.
+	crops_remaining -= 1
+
+	# Produce one seed pack.
+	seeds_ready += 1
+
+	print("Seed pack produced.")
+	print("Remaining crops: ", crops_remaining)
+	print("Seeds waiting: ", seeds_ready)
+
+	emit_signal("seed_produced")
+	emit_signal("state_changed")
+
+	# Continue if more crops remain.
+	if crops_remaining > 0:
+		timer.start(process_time)
+	else:
+		print("Processing complete.")
+		emit_signal("processing_finished")
+
+
+func collect() -> void:
+
+	if seeds_ready == 0:
+		print("Nothing to collect.")
+		return
+
+	# TODO:
+	# Inventory.add_item(output_seed, seeds_ready)
+
+	print("Collected ", seeds_ready, " seed packs.")
+
+	seeds_ready = 0
+
+	emit_signal("seeds_collected")
+	emit_signal("state_changed")
+
+	update_machine_state()
+
+
+func update_machine_state() -> void:
+
+	# Machine becomes idle only after:
+	# 1. Every crop has been processed.
+	# 2. Every produced seed pack has been collected.
+	if crops_remaining == 0 and seeds_ready == 0:
+
+		timer.stop()
+
+		input_crop = null
+		output_seed = null
+
+		state = State.IDLE
+
+		print("Seed Maker is idle.")
+
+		emit_signal("state_changed")
+
 
 
 func _ready():
@@ -85,3 +167,6 @@ func _on_interaction_collider_body_exited(body: Node2D) -> void:
 		#get_tree().current_scene.add_child(ui_instance)
 #
 	#ui_instance.open(self)
+
+
+# Bottom
--- GameFiles/scenes/machines/seed_maker_ui.gd
+++ GameFiles/scenes/machines/seed_maker_ui.gd
@@ -2,73 +2,118 @@ extends popup_ui
 
 @export var machine_obj: StaticBody2D
 
-
 @onready var slot_in: Panel = $PopupRoot/Panel/panel_left/slot_in
-#@onready var slot_in_label: Label = $PopupRoot/Panel/panel_left/Label
 @onready var slot_out: Panel = $PopupRoot/Panel/panel_left/slot_out
-#@onready var slot_out_label: Label = $PopupRoot/Panel/panel_left/Label2
 @onready var grid_container: GridContainer = $PopupRoot/Panel/panel_right/GridContainer
 @onready var progress_bar: ProgressBar = $PopupRoot/Panel/ProgressBar
 @onready var btn_collect: Button = $PopupRoot/Panel/btnCOLLECT
 
+# Temporary until inventory integration
 var selected_crop = preload("res://resources/crop_carrot.tres")
+var selected_amount := 1
 
 
+func _ready() -> void:
 
-#func _ready() -> void:
-	#slot_in_label.text = ""
-	#slot_out_label.text = ""
+	if machine_obj == null:
+		push_error("Seed Maker UI: machine_obj is not assigned.")
+		return
 
-func _process(delta):
-	
+	# Connect to machine signals
+	machine_obj.processing_started.connect(update_ui)
+	machine_obj.seed_produced.connect(update_ui)
+	machine_obj.processing_finished.connect(update_ui)
+	machine_obj.seeds_collected.connect(update_ui)
+	machine_obj.state_changed.connect(update_ui)
+
+	update_ui()
+
+
+func _process(_delta: float) -> void:
+
 	if machine_obj == null:
 		return
-	
-	progress_bar.visible = machine_obj.state == machine_obj.State.PROCESSING
-	
+
 	if machine_obj.state == machine_obj.State.PROCESSING:
+		progress_bar.visible = true
+
 		progress_bar.value = (
 			(machine_obj.timer.wait_time - machine_obj.timer.time_left)
 			/ machine_obj.timer.wait_time
-		) * 100
-		
-	btn_collect.visible = machine_obj.state == machine_obj.State.FINISHED
+		) * 100.0
+	else:
+		progress_bar.visible = false
+		progress_bar.value = 0
 
 
+func update_ui() -> void:
+
+	btn_collect.visible = machine_obj.seeds_ready > 0
+
+	update_input_slot()
+	update_output_slot()
+
+	# Optional labels later
+	# lbl_remaining.text = str(machine_obj.crops_remaining)
+	# lbl_ready.text = str(machine_obj.seeds_ready)
 
 
 func _on_button_pressed() -> void:
-	machine_obj.start_processing(selected_crop)
+
+	machine_obj.start_processing(selected_crop, selected_amount)
+
 
 func _on_btn_collect_pressed() -> void:
+
 	machine_obj.collect()
-	machine_obj.add_seeds()
 
 
-# TODO: take icon from selected resource and put in slot_in
-# TODO: when it finishes, take icon and put in slot_out
-# TODO: Add error checking and logic validation steps
+func update_input_slot() -> void:
+
+	var texture_rect = slot_in.get_child(0).get_child(0)
+
+	if machine_obj.input_crop:
+		texture_rect.texture = machine_obj.input_crop.icon
+	else:
+		texture_rect.texture = null
 
 
+func update_output_slot() -> void:
+
+	var texture_rect = slot_out.get_child(0).get_child(0)
+
+	if machine_obj.output_seed:
+		texture_rect.texture = machine_obj.output_seed.icon
+	else:
+		texture_rect.texture = null
+
+
+func _move_selected_to_in() -> void:
+
+	var gv_items = grid_container.get_children()
+
+	if gv_items.is_empty():
+		return
+
+	var item_from_grid = gv_items[0].get_child(0).get_child(0)
+	var destination = slot_in.get_child(0).get_child(0)
+
+	destination.texture = item_from_grid.texture
 
 
 func _on_button_2_pressed() -> void:
+
 	_reset()
 
 
+func _reset() -> void:
+
+	var input_texture = slot_in.get_child(0).get_child(0)
+	input_texture.texture = null
+
+	var output_texture = slot_out.get_child(0).get_child(0)
+	output_texture.texture = null
 
 
-func _move_selected_to_in():
-	var gv_itms = grid_container.get_children()
-	var item_from_gv = gv_itms[0].get_child(0).get_child(0)
-	var dest_slot    = slot_in.get_child(0).get_child(0)
-	dest_slot.texture = item_from_gv.texture
-	print("test")
 
-func _reset():
-	var src_slot    = slot_in.get_child(0).get_child(0)
-	src_slot.texture = null
-	var dest_slot   = slot_out.get_child(0).get_child(0)
-	dest_slot.texture = null
-	#slot_in_label.text = ""
-	#slot_out_label.text = ""
+# Bottom
